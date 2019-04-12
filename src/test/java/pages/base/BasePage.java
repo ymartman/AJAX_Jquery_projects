9e5decc3059899ffd7e0a8f808c6337e1d8ae617
ymartman <ymartman@users.noreply.github.com>
2019-04-12 13:17:48 +0300

    Update BasePage.java

--- src/test/java/pages/base/BasePage.java
+++ src/test/java/pages/base/BasePage.java
@@ -3,6 +3,8 @@ package pages.base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+//Зачем он нужен? Если у тебя конструктор всё равно везде переписывается
+//Зачем передавать в конструктор драйвер?
 public class BasePage {
 
     protected BasePage(WebDriver driver){
